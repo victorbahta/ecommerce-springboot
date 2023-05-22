ce3b5afcb292830f06502444ead84a83a8d52c29
Salm Do <thaodo.usa@gmail.com>
2023-05-22 18:01:34 -0500

    delete target

---
+++ product/src/main/java/edu/miu/cs/dto/ReviewDTO.java
@@ -0,0 +1,20 @@
+package edu.miu.cs.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+public class ReviewDTO {
+    private int id;
+
+    private String title;
+    private String description;
+    private int numberOfStar;
+    private Date reviewDate;
+    private String reviewerFirstname;
+    private String reviewerLastname;
+}
