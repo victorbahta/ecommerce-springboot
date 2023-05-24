ef790963f610120b63f5c6c87ad49c50955e506d
Salm Do <thaodo.usa@gmail.com>
2023-05-24 10:28:44 -0500

    update product service

--- product/src/main/java/edu/miu/cs/services/ReviewServiceImp.java
+++ product/src/main/java/edu/miu/cs/services/ReviewServiceImp.java
@@ -1,15 +1,15 @@
 package edu.miu.cs.services;
 
-import com.netflix.discovery.converters.Auto;
 import edu.miu.cs.domains.ComponentProduct;
 import edu.miu.cs.domains.Review;
 import edu.miu.cs.domains.Reviewer;
-import edu.miu.cs.dto.CustomerDto;
-import edu.miu.cs.dto.ProductDTO;
+import edu.miu.cs.dto.CustomerDTO;
 import edu.miu.cs.dto.ReviewDTO;
 import edu.miu.cs.feign.CustomerService;
+import edu.miu.cs.feign.OrderService;
 import edu.miu.cs.repositories.ComponentProductRepository;
 import edu.miu.cs.repositories.ReviewRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,8 +20,10 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Slf4j
 public class ReviewServiceImp implements ReviewService{
     @Autowired
     private ComponentProductRepository componentProductRepository;
@@ -31,16 +33,35 @@ public class ReviewServiceImp implements ReviewService{
     private ModelMapper modelMapper;
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private OrderService orderService;
     @Override
     public ReviewDTO addReview(int productId, ReviewDTO reviewDTO) {
         try {
             ComponentProduct product = componentProductRepository.getReferenceById(productId);
-            CustomerDto customer = customerService.getCustomerById(reviewDTO.getReviewerId());
-            System.out.println(customer);
-
             if (product == null) {
+                log.error("Could not find the product " + productId+ " by id");
                 return new ReviewDTO();
             }
+
+            Optional<Integer> reviewCount = reviewRepository.countDistinctByProductAndOrderIdAndReviewer(productId
+                    , reviewDTO.getOrderId(), reviewDTO.getReviewerId());
+            if (!reviewCount.isPresent() || reviewCount.get() == 1) {
+                log.error("Customer " + reviewDTO.getReviewerId() + " could not review a product " + productId + " of order " + reviewDTO.getOrderId() + " twice");
+                return new ReviewDTO();
+            }
+
+            Optional<Boolean> isBoughtProduct = orderService.checkProductOrderedByCustomer(reviewDTO.getReviewerId()
+                    , reviewDTO.getOrderId(), productId);
+            System.out.println(isBoughtProduct);
+
+            if (!isBoughtProduct.isPresent() || !isBoughtProduct.get()) {
+                log.error("Customer " + reviewDTO.getReviewerId() + " could only review purchased products");
+                return new ReviewDTO();
+            }
+
+            CustomerDTO customer = customerService.getCustomerById(reviewDTO.getReviewerId());
+
             Review review = modelMapper.map(reviewDTO, Review.class);
             review.setReviewDate(new Date());
             review.setProduct(product);
@@ -60,7 +81,13 @@ public class ReviewServiceImp implements ReviewService{
     public Page<ReviewDTO> getAllReviews(int productId, Pageable pageable) {
         try {
             var result = reviewRepository.findReviewsByProduct(productId, pageable);
-            return result.map(review -> modelMapper.map(review, ReviewDTO.class));
+            if (result.isPresent()) {
+                return result.get().map(review -> modelMapper.map(review, ReviewDTO.class));
+            } else {
+                log.error("Could not find the review of product " + productId);
+                List<ReviewDTO> emptyList = new ArrayList<>();
+                return new PageImpl<ReviewDTO>(emptyList, pageable, 0);
+            }
         } catch (Exception ex){
             List<ReviewDTO> emptyList = new ArrayList<>();
             return new PageImpl<ReviewDTO>(emptyList, pageable, 0);
