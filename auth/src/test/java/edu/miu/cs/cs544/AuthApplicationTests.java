5d1e361a96f04848983c5fcda62d9fa6f8d981b3
Manh Tien Dinh <dinhmanhtien@gmail.com>
2023-05-24 14:13:33 -0500

    update Authentication service

---
+++ auth/src/test/java/edu/miu/cs/cs544/AuthApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.miu.cs.cs544;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
